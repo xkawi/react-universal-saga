260ca4b16dd28c9f07e0c999f50b403ded04308a
xkawi <kawixiao@gmail.com>
2016-07-23 08:53:24 +0800

    update description in config.js and clean up routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,32 +7,7 @@ import {
   RepoPage
 } from 'containers';
 
-/**
- * currently, "store" variable is unused
- * might need to use once auth flow is implemented
- * NOTE: eslint suggested that, if we use arrow function with curly braces,
- * the body must be more than 1 line (http://eslint.org/docs/rules/arrow-body-style);
- */
-
 export default (store) => {
-  // eslint-disable-next-line no-unused-vars
-  const requireLogin = (nextState, replace, cb) => {
-    function checkAuth() {
-      const { auth: { isAuthenticated } } = store.getState();
-      if (!isAuthenticated) {
-        // oops, not logged in, so can't be here!
-        replace('/');
-      }
-      cb();
-    }
-    // Optimization: if already login, try to refresh
-    checkAuth();
-    // if (!isAuthLoaded(store.getState())) {
-    //   store.dispatch(loadAuth()).then(checkAuth);
-    // } else {
-    // }
-  };
-
   return (
     <Route path="/" component={App}>
       <Route path="/:login" component={UserPage} />
