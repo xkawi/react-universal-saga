5d9c969febf54e2c75f2a8c80bfc9299b3a3c2d5
Mehdi Zonjy <mehdizonjy@hotmail.com>
2016-10-12 02:47:31 +0200

    optimized react-bootstrap import

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Button } from 'react-bootstrap';
+import  Button  from 'react-bootstrap/lib/Button';
 
 const GITHUB_REPO = 'https://github.com/xkawi/react-universal-saga';
 
